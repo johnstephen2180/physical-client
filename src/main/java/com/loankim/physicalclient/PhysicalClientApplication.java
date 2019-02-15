6ce775cceac0b2d949d0f5fe14397341a005aa7f
johnstephen2180 <john.stephen.2180@gmail.com>
2019-02-15 23:14:57 +0700

    First

---
+++ src/main/java/com/loankim/physicalclient/PhysicalClientApplication.java
@@ -0,0 +1,14 @@
+package com.loankim.physicalclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysicalClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysicalClientApplication.class, args);
+	}
+
+}
+
